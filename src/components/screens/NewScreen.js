2d8d73909d02f7ed5f789ab292d801d080bfcd17
sozturk <semih.1991.ozturk@gmail.com>
2021-05-21 10:30:57 +0300

    NewScreen bg-color corrected

--- src/components/screens/NewScreen.js
+++ src/components/screens/NewScreen.js
@@ -17,111 +17,108 @@ const NewScreen = () => {
         <Add height={35} width={25} fill={"#ABA8BA"} />
       </View>
 
-      <View>
-        <View style={{ backgroundColor: "#FAF2EA", marginTop: 25, paddingVertical: 15, borderRadius: 20 }}>
-          <Text style={[styles.normalText]}>Select time</Text>
+      <View style={{ backgroundColor: "#FAF2EA", marginTop: 25, paddingVertical: 15, borderRadius: 20 }}>
+        <Text style={[styles.normalText]}>Select time</Text>
 
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ marginTop: 15 }}>
-            <View style={{ backgroundColor: "black", padding: 10, borderRadius: 13 }}>
-              <Text style={{ color: "white", }}>15:00</Text>
-            </View>
-            <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
-              <Text style={{ color: "#B4AEB0" }}>15:00</Text>
-            </View>
-            <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
-              <Text style={{ color: "#B4AEB0" }}>15:00</Text>
-            </View>
-            <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
-              <Text style={{ color: "#B4AEB0" }}>15:00</Text>
-            </View>
-            <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
-              <Text style={{ color: "#B4AEB0" }}>15:00</Text>
-            </View>
-            <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
-              <Text style={{ color: "#B4AEB0" }}>15:00</Text>
-            </View>
-            <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
-              <Text style={{ color: "#B4AEB0" }}>15:00</Text>
-            </View>
-            <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
-              <Text style={{ color: "#B4AEB0" }}>15:00</Text>
-            </View>
-          </ScrollView>
-        </View>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{ marginTop: 15 }}>
+          <View style={{ backgroundColor: "black", padding: 10, borderRadius: 13 }}>
+            <Text style={{ color: "white", }}>15:00</Text>
+          </View>
+          <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
+            <Text style={{ color: "#B4AEB0" }}>15:00</Text>
+          </View>
+          <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
+            <Text style={{ color: "#B4AEB0" }}>15:00</Text>
+          </View>
+          <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
+            <Text style={{ color: "#B4AEB0" }}>15:00</Text>
+          </View>
+          <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
+            <Text style={{ color: "#B4AEB0" }}>15:00</Text>
+          </View>
+          <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
+            <Text style={{ color: "#B4AEB0" }}>15:00</Text>
+          </View>
+          <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
+            <Text style={{ color: "#B4AEB0" }}>15:00</Text>
+          </View>
+          <View style={{ backgroundColor: "#F4EAE2", padding: 10, borderRadius: 13, marginLeft: 25 }}>
+            <Text style={{ color: "#B4AEB0" }}>15:00</Text>
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={{ marginVertical: 25 }}>
+        <Text style={styles.normalText}>Select type of class</Text>
 
         <View style={{ marginVertical: 25 }}>
-          <Text style={styles.normalText}>Select type of class</Text>
-
-          <View style={{ marginVertical: 25 }}>
-            <View style={{ backgroundColor: "black", paddingVertical: 20, borderRadius: 20 }} >
-              <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                  <View>
-                    <Person height={35} width={25} fill={"#8964F6"} />
-                  </View>
-                  <View style={{ marginLeft: 15 }}>
-                    <Text style={{ color: "#FEFEFE", fontSize: 18 }}>Chemistry Class</Text>
-                    <Text style={{ color: "#F4F3F4", fontSize: 17 }}>2 - 20 Students</Text>
-                  </View>
-
+          <View style={{ backgroundColor: "black", paddingVertical: 20, borderRadius: 20 }} >
+            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View>
+                  <Person height={35} width={25} fill={"#8964F6"} />
                 </View>
-
-                <View style={{ marginRight: 20 }}>
-                  <Text style={styles.coin}>5 Coins</Text>
+                <View style={{ marginLeft: 15 }}>
+                  <Text style={{ color: "#FEFEFE", fontSize: 18 }}>Chemistry Class</Text>
+                  <Text style={{ color: "#F4F3F4", fontSize: 17 }}>2 - 20 Students</Text>
                 </View>
-              </View>
-            </View>
-
-
-            <View style={{ backgroundColor: "#FFFFFF", paddingVertical: 20, borderRadius: 20 }}>
-              <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                  <View>
-                    <Person height={35} width={25} fill={"#F0A02E"} />
-                  </View>
-                  <View style={{ marginLeft: 15 }}>
-                    <Text style={{ color: "#BFBDC6", fontSize: 18 }}>Philosopy Class</Text>
-                    <Text style={{ color: "#BDBCC3", fontSize: 17 }}>&#60; 1000 Students</Text>
-                  </View>
-
-                </View>
-
-                <View style={{ marginRight: 20 }}>
-                  <Text style={styles.coin}>5 Coins</Text>
-                </View>
-              </View>
-            </View>
-
-          </View>
-
-          <View>
-            <Text style={styles.normalText}>Summary</Text>
-
-            <View style={{ elevation: 5 }}>
-              <View style={styles.summary}>
-                <Text style={styles.summaryText}>Schedule</Text>
-                <Text style={{ fontSize: 17, fontWeight: "bold" }}>15:00</Text>
 
               </View>
-              <View style={styles.summary}>
-                <Text style={styles.summaryText}>Coin</Text>
+
+              <View style={{ marginRight: 20 }}>
                 <Text style={styles.coin}>5 Coins</Text>
-
-              </View>
-              <View style={styles.summary}>
-                <Text style={styles.summaryText}>Class type</Text>
-                <Text style={{ fontSize: 17, fontWeight: "bold" }}>Small Class</Text>
-
               </View>
             </View>
           </View>
 
+
+          <View style={{ backgroundColor: "#FFFFFF", paddingVertical: 20, borderRadius: 20 }}>
+            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View>
+                  <Person height={35} width={25} fill={"#F0A02E"} />
+                </View>
+                <View style={{ marginLeft: 15 }}>
+                  <Text style={{ color: "#BFBDC6", fontSize: 18 }}>Philosopy Class</Text>
+                  <Text style={{ color: "#BDBCC3", fontSize: 17 }}>&#60; 1000 Students</Text>
+                </View>
+
+              </View>
+
+              <View style={{ marginRight: 20 }}>
+                <Text style={styles.coin}>5 Coins</Text>
+              </View>
+            </View>
+          </View>
+
+        </View>
+
+        <View>
+          <Text style={styles.normalText}>Summary</Text>
+
+          <View style={{ elevation: 5 }}>
+            <View style={styles.summary}>
+              <Text style={styles.summaryText}>Schedule</Text>
+              <Text style={{ fontSize: 17, fontWeight: "bold" }}>15:00</Text>
+
+            </View>
+            <View style={styles.summary}>
+              <Text style={styles.summaryText}>Coin</Text>
+              <Text style={styles.coin}>5 Coins</Text>
+
+            </View>
+            <View style={styles.summary}>
+              <Text style={styles.summaryText}>Class type</Text>
+              <Text style={{ fontSize: 17, fontWeight: "bold" }}>Small Class</Text>
+
+            </View>
+          </View>
         </View>
 
       </View>
 
-      <View style={{ backgroundColor: "black", paddingVertical: 5, borderRadius: 10, justifyContent: "center", flexDirection:"row"}}>
-        <Text style={{ fontSize:16, color:"white",  }}>Join</Text>
+      <View style={{ backgroundColor: "black", paddingVertical: 5, borderRadius: 10, justifyContent: "center", flexDirection: "row" }}>
+        <Text style={{ fontSize: 16, color: "white", }}>Join</Text>
 
       </View>
 
@@ -133,7 +130,7 @@ const NewScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFF6EC",
+    backgroundColor: "#FFFFFF",
     padding: 25,
   },
   icon: {
@@ -156,6 +153,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginTop: 10,
+    elevation:7,
   },
   summaryText: {
     fontSize: 17,
