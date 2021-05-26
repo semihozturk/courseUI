347aa3a864a65febeb0064de7cac3b1e9a43a534
sozturk <semih.1991.ozturk@gmail.com>
2021-05-26 07:21:27 +0300

    added AnotherScreen by using Dimensions.get(). and flexWrap

--- src/components/screens/AnotherScreen.js
+++ src/components/screens/AnotherScreen.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Dimensions, Text, View } from "react-native";
 
 const AnotherScreen = () => {
   return(
-    <View>
-      <Text>Hello World</Text>
-
+    <View style={{ top:Dimensions.get("window").height/2, flex:1, flexWrap:"wrap", flexDirection:"row", paddingHorizontal:20 }}>
+      <View style={{ width: (Dimensions.get("window").width - 40) / 3, height: (Dimensions.get("window").width - 40) / 3 }}>
+        <View style={{ backgroundColor: "orange", borderRadius:30, flex: 1, margin: 5 }}></View>
+      </View>
+      <View style={{ width: (Dimensions.get("window").width - 40) / 3, height: (Dimensions.get("window").width - 40) / 3 }}>
+        <View style={{ backgroundColor: "orange", borderRadius: 30, flex: 1, margin: 5 }}></View>
+      </View>
+      <View style={{ width: (Dimensions.get("window").width - 40) / 3, height: (Dimensions.get("window").width - 40) / 3 }}>
+        <View style={{ backgroundColor: "orange", borderRadius: 30, flex: 1, margin: 5 }}></View>
+      </View>
+      <View style={{ width: (Dimensions.get("window").width - 40) / 3, height: (Dimensions.get("window").width - 40) / 3 }}>
+        <View style={{ backgroundColor: "orange", borderRadius: 30, flex: 1, margin: 5 }}></View>
+      </View>
+      <View style={{ width: (Dimensions.get("window").width - 40) / 3, height: (Dimensions.get("window").width - 40) / 3 }}>
+        <View style={{ backgroundColor: "orange", borderRadius: 30, flex: 1, margin: 5 }}></View>
+      </View>
+      <View style={{ width: (Dimensions.get("window").width - 40) / 3, height: (Dimensions.get("window").width - 40) / 3 }}>
+        <View style={{ backgroundColor: "orange", borderRadius: 30, flex: 1, margin: 5 }}></View>
+      </View>
     </View>
   )
 };
